c334b69e8a484dc3030a0b502a00868a87128fe1
hermonaz <hermon.nigatu@gmail.com>
2021-12-19 01:31:18 -0800

    refactor:added a comment line before the code

--- projectbikeshare.py
+++ projectbikeshare.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 import datetime
+#The data is made up of 3 cities
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
